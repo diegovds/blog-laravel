82fffe20c59ca3d6eecf6103d159557820b8ab85
Diego Viana <dsantos@inf.ufsm.br>
2025-07-26 22:28:38 -0300

    adding the user creation route

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,34 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/ping', function() {
-    return 'pong';
+Route::post('/auth/signup', function(Request $request) {
+
+    // Validar os dados:
+
+    $request -> validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6',
+    ]);
+
+    $user = User::create([
+        'name' => $request -> name,
+        'email' => $request -> email,
+        'password' => Hash::make($request -> password),
+    ]);
+
+    $returnData = [];
+    $returnData['user'] = $user;
+    $returnData['token'] = $user -> createToken($user -> id. '-' .$user -> email) -> plainTextToken;
+
+    return $returnData;
+
 });

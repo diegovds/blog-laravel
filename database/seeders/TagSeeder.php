d92d8b7d675d4a60d239d7636dcab94dc21134ea
Diego Viana <dsantos@inf.ufsm.br>
2025-07-28 01:24:58 -0300

    Update TagSeeder.php

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -18,5 +18,11 @@ class TagSeeder extends Seeder
         $tag2 = Tag::create(['name' => 'Laravel (tag2)']);
         $tag3 = Tag::create(['name' => 'JavaScript (tag3)']);
         $tag4 = Tag::create(['name' => 'TypeScript (tag4)']);
+        $tag5 = Tag::create(['name' => 'HTML (tag5)']);
+        $tag6 = Tag::create(['name' => 'CSS (tag6)']);
+        $tag7 = Tag::create(['name' => 'MySQL (tag7)']);
+        $tag8 = Tag::create(['name' => 'React (tag8)']);
+        $tag9 = Tag::create(['name' => 'Vue.js (tag9)']);
+        $tag10 = Tag::create(['name' => 'Node.js (tag10)']);
     }
 }
